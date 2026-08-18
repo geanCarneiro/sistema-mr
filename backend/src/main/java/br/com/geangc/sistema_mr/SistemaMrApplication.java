16518a25885d8aeb2bd921673aa8c7e23b8f30b7
geanCarneiro <geancarneiro1@gmail.com>
2026-08-18 08:19:19 -0300

    Initial commit

---
+++ backend/src/main/java/br/com/geangc/sistema_mr/SistemaMrApplication.java
@@ -0,0 +1,13 @@
+package br.com.geangc.sistema_mr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/geangc/sistema_mr/SistemaMrApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.geangc.sistema_mr;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMrApplicationTests {
+
+	@Test
+        @Disabled("Desabilitado temporariamente até configurarmos os mocks de infra")
+	void contextLoads() {
+	}
+
+}
